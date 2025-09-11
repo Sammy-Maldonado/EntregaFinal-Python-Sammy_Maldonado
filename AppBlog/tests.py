b3430a43d350384634a1f5e7e8095531d76e5caa
Sammy Maldonado <sammyalejandro.m.p@gmail.com>
2025-09-10 22:38:01 -0300

    test.py en AppBlog configurado

--- AppBlog/tests.py
+++ AppBlog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
+from AppBlog.views import PostCreateView
+from .models import Post, User
 
-# Create your tests here.
+class PostTests(TestCase):
+    def setup(self):
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.post = Post.objects.create(titulo="Python", auto=self.user)
+
+#Esta prueba unitaria que verifica que lo que hace el codigo escrito en el primer parametro, hace lo que se define en el segundo parametro. En este caso, verifica que la URL de éxito de la vista PostCreateView es igual a la URL resuelta para "AppBlog:post_list".
+class PostCreateViewTests(TestCase):
+    def test_success_url(self):
+        url = reverse_lazy("AppBlog:post_list")
+        self.assertEqual(PostCreateView.success_url, url)
